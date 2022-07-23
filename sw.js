9f619caedf534e59df062cd4f8eb9b8653064205
Ahmad Sajadian <ahmadx87@gmail.com>
2022-07-24 01:28:26 +0430

    log push event data

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
 self.addEventListener("push", function (event) {
+  console.log(event)
+   const data = event.data?.json() ?? {};
+   console.log(data);
   const promiseChain = self.registration.showNotification("Hello, World.")
 
   event.waitUntil(promiseChain)
